594befc6b3b00e0846ca9b3ee3bd6cde01d81794
Mohamed A. Abdallah <Eng.Mohamed.A.Abdallah@gmail.com>
2025-08-10 21:31:21 +0300

    Refactor fetchData function to implement caching mechanism for API responses

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 app.use(cors());
@@ -15,35 +16,70 @@ app.get("/", async (req, res) => {
   }
 });
 
+async function fetchData(req, res) {
+  try {
+    let cache = fs.readFileSync("./cache.json", "utf-8");
+    if (cache) {
+      try {
+        cache = JSON.parse(cache);
+        // Check if cache is still valid
+        if (
+          cache.timestamp &&
+          Date.now() - cache.timestamp < process.env.CACHE_TIMEOUT * 1000
+        ) {
+          console.log(`Used Cache ${cache.timestamp} ${Date.now()}`);
+          return res.json(cache.data);
+        }
+      } catch (e) {
+        console.error("Error parsing cache:", e);
+      }
+    }
+  } catch (e) {
+    console.error("Error reading cache:", e);
+  }
+
+  const r = await fetch(process.env.END_POINT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      RES_Request: {
+        Request_Type: "Bookings",
+        Authentication: {
+          HotelCode: process.env.HOTEL_CODE,
+          AuthCode: process.env.AUTH_CODE,
+        },
+      },
+    }),
+  });
+
+  if (!r.ok) {
+    console.error("VENDOR API Error Details:", data);
+    return res.status(r.status).json({
+      error: "Upstream error",
+      status: r.status,
+      details: data,
+    });
+  }
+
+  const data = await r.json();
+  const cache = { timestamp: Date.now(), data };
+
+  fs.writeFile("./cache.json", JSON.stringify(cache), (err) => {
+    if (err) {
+      console.error("Error writing cache to disk:", err);
+    }
+  });
+}
+
 app.get("/api", async (req, res) => {
   try {
-    const r = await fetch(process.env.END_POINT_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        RES_Request: {
-          Request_Type: "Bookings",
-          Authentication: {
-            HotelCode: process.env.HOTEL_CODE,
-            AuthCode: process.env.AUTH_CODE,
-          },
-        },
-      }),
-    });
-
-    if (!r.ok) {
-      console.error("VENDOR API Error Details:", data);
-      return res.status(r.status).json({
-        error: "Upstream error",
-        status: r.status,
-        details: data,
-      });
+    const data = await fetchData(req, res);
+    if (!data) {
+      console.log(`Data | ${data}`);
+      return res.status(500).json({ error: "Failed to fetch data" });
     }
-
-    const data = await r.json();
-
     const Reservations = data.Reservations;
 
     const Reservation = Reservations.Reservation;
